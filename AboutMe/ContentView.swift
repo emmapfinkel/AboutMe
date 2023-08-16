9f1a51ca7a081a62388b4693aa900a26bd7ad2eb
Emma Finkel <emma.finkel@gmail.com>
2023-08-16 10:43:25 -0400

    about me app from training

--- AboutMe/ContentView.swift
+++ AboutMe/ContentView.swift
@@ -8,14 +8,52 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var textTitle1 = " "
+    @State private var textTitle2 = " "
+
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+     
+        ZStack {
+            
+            Color(.systemYellow)
+                .ignoresSafeArea()
+            
+            VStack(alignment: .center, spacing: 20.0) {
+                
+                Text("All About Emma")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .padding()
+                
+                Button("Fact 1") {
+                    textTitle1 = "I love to read. 📚"
+                }
+                .padding()
+                
+                Text(textTitle1)
+                    .font(.title)
+                
+                Button("Fact 2") {
+                    textTitle2 = "I love to play tennis! 🎾"
+                }
+                
+                Text(textTitle2)
+                    .font(.title)
+                    .padding()
+                
+                
+            }
+            .padding()
+            .background(Rectangle()
+                .foregroundColor(.white)
+                .cornerRadius(10)
+                .shadow(radius: 10)
+                .padding()
+            )
         }
-        .padding()
+        
     }
 }
 
